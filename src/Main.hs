8d04645a1d7b872e29e7649d6551f31263cb4c83
keresztesbeata <keresztesbeata00@yahoo.com>
2022-01-09 04:11:00 +0200

    implemented earch and add new item

--- src/Main.hs
+++ src/Main.hs
@@ -36,54 +36,81 @@ usageMsg =
 -- | Handle the init command
 handleInit :: TestableMonadIO m => m ()
 handleInit = do
-              savedDB <- DB.save DB.empty
-              case savedDB of
-                  (Error err) -> print err
-                  (Success db) -> return ()
+              let newEmptyDB = DB.empty
+              DB.save newEmptyDB
+              return ();
 
 -- | Handle the get command
 handleGet :: TestableMonadIO m => GetOptions -> m ()
 handleGet getOpts = do
-                  db <- DB.load
-                  case db of
-                    (Error err) -> print err
-                    (Success snippetDB) ->
-                        let
-                          foundEntry = DB.findFirst (\entry -> entryId entry == getOptId getOpts) snippetDB
-                        in
-                          case foundEntry of
-                            Nothing -> print "No entry with given id was found"
-                            Just entry -> print $ show (FmtEntry entry)
+  loadedDB <- DB.load
+  case loadedDB of
+    (Error err) -> 
+      print err
+    (Success db) ->
+      let
+        foundEntry = DB.findFirst (\entry -> entryId entry == getOptId getOpts) db
+      in
+        case foundEntry of
+            Nothing -> 
+              print "No entry was found"
+            Just entry -> 
+              print $ show (FmtEntry entry)
 
 -- | Handle the search command
 handleSearch :: TestableMonadIO m => SearchOptions -> m ()
 handleSearch searchOpts = do
-                      db <- DB.load
-                      case db of
-                        (Error err) -> print err
-                        (Success snippetDB) ->
-                          let
-                            foundEntries = DB.findAll (matchedByAllQueries (searchOptTerms searchOpts)) snippetDB
-                            in
-                              case foundEntries of
-                                [] -> print "No entries found"
-                                _ -> print $ show (fmap FmtEntry foundEntries)
+  db <- DB.load
+  case db of
+    (Error err) -> 
+      print err
+    (Success snippetDB) ->
+      let
+        foundEntries = DB.findAll (matchedByAllQueries (searchOptTerms searchOpts)) snippetDB
+      in
+        case foundEntries of
+          [] -> 
+            print "No entries found"
+          _ -> 
+            print $ (show . fmap FmtEntry) foundEntries
 
 -- | Handle the add command
 handleAdd :: TestableMonadIO m => AddOptions -> m ()
-handleAdd addOpts =
-  return ()
-  where
-    makeEntry :: Int -> String -> AddOptions -> Entry
-    makeEntry id snippet addOpts =
-      Entry
-        { entryId = id,
-          entrySnippet = snippet,
-          entryFilename = addOptFilename addOpts,
-          entryLanguage = addOptLanguage addOpts,
-          entryDescription = addOptDescription addOpts,
-          entryTags = addOptTags addOpts
-        }
+handleAdd addOpts = do
+  fileContent <- readFile (addOptFilename addOpts)
+  loadedDB <- DB.load
+  let
+    db = getSuccess loadedDB DB.empty
+    existingEnity = DB.findFirst (\entry -> entry == addOptsToEntry (entryId entry) fileContent addOpts) db
+    in
+      case existingEnity of
+        Just _ -> print "Entry with this content already exists: " 
+        Nothing -> do
+                      DB.modify (DB.insertWith (\id -> makeEntry id fileContent addOpts))
+                      return ()
+    where
+      makeEntry :: Int -> String -> AddOptions -> Entry
+      makeEntry id snippet addOpts =
+        Entry
+          { entryId = id,
+            entrySnippet = snippet,
+            entryFilename = addOptFilename addOpts,
+            entryLanguage = addOptLanguage addOpts,
+            entryDescription = addOptDescription addOpts,
+            entryTags = addOptTags addOpts
+          }
+
+addOptsToEntry :: Int -> String -> AddOptions -> Entry
+addOptsToEntry id snippet addOpts =
+  Entry
+  { entryId = id
+  , entrySnippet = snippet
+  , entryFilename = addOptFilename addOpts
+  , entryLanguage = addOptLanguage addOpts
+  , entryDescription = addOptDescription addOpts
+  , entryTags = addOptTags addOpts
+  }
+
 
 -- | Dispatch the handler for each command
 run :: TestableMonadIO m => Args -> m ()
